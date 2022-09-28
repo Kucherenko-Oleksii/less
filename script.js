3e802932888c223637748887ac346636dd4b00d7
Oleksii <kucherenkoolexiy@gmail.com>
2022-09-28 11:52:09 +0300

    update_files

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Hi, i'm alert)");
+const div = document.querySelector("#container");
+const card = document.querySelector("#aside_container");
+
+div.addEventListener("click", ()=>{
+    div.style.backgroundColor = "aqua";
+});
+
+
+card.addEventListener('dblclick', () => {
+  card.classList.toggle('large');
+});
